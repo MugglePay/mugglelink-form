4290f150114c7f7b5eddf85b092ea3366000e0f8
Harshita <106453092+Harshita0008@users.noreply.github.com>
2024-02-28 19:26:51 +0530

    Update MerchantForm.tsx

--- src/components/MerchantForm.tsx
+++ src/components/MerchantForm.tsx
@@ -69,7 +69,6 @@ const MerchantForm = ({ insertApi }: { insertApi: any }) => {
       product_image_url : "",
       currency_option: "USD",
       price: "",
-      payment_option: "USDT-Arbitrum 0.1",
       escrow_enabled: false,
       require_full_name: false,
       require_email: false,
